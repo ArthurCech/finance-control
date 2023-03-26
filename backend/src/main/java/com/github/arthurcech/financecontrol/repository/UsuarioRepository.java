ef8f21b225e6272f4df4cfff7ae9b4a1eeec664f
Arthur Cech Francisco <arthurcech@gmail.com>
2023-03-26 09:06:34 -0300

    feat: create Usuario, Permissao e UsuarioRepository

---
+++ backend/src/main/java/com/github/arthurcech/financecontrol/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.github.arthurcech.financecontrol.repository;
+
+import com.github.arthurcech.financecontrol.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
---
+++ backend/src/main/java/com/github/arthurcech/financecontrol/security/AppUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.github.arthurcech.financecontrol.security;
+
+import com.github.arthurcech.financecontrol.domain.Usuario;
+import com.github.arthurcech.financecontrol.repository.UsuarioRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AppUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+        return new User(email, usuario.getSenha(), getPermissoes(usuario));
+    }
+
+    private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+        return usuario.getPermissoes().stream()
+                .map(permissao -> new SimpleGrantedAuthority(permissao.getDescricao().toUpperCase()))
+                .collect(Collectors.toSet());
+    }
+
+}
